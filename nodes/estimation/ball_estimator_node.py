aedc7648d8da3c17f60aa78558e7b3bde1d83586
Parker Lusk <parkerclusk@gmail.com>
2017-01-07 15:02:00 -0700

    Python 'is not' is not the same as '!=', fyi

--- nodes/estimation/ball_estimator_node.py
+++ nodes/estimation/ball_estimator_node.py
@@ -14,14 +14,22 @@ and patch it through as a BallState message.
 """
 
 _state_pub = None
-_we_are_home = True
+_team_side = None
+
+# Again, this could be done in the vision code
+from soccerref.msg import GameState
+_game_state = GameState
+
+def _handle_game_state(msg):
+    global _game_state
+    _game_state = msg
 
 def _handle_vision_ball_position(msg):
     # Flip the coordinate system so that our side is always the negative
     # side. This is nice because we won't have to think about this later
     # in our high-level AI code and elsewhere.
     # This could also be done in the vision node; see original mcthuggets code
-    flip = 1 if _we_are_home else -1
+    flip = -1 if (_team_side !='home') ^ bool(_game_state.second_half) else 1
 
     # Construct ball_state message, BallState
     new_msg = BallState()
@@ -36,14 +44,16 @@ def _handle_vision_ball_position(msg):
 def main():
     rospy.init_node('ball_estimator', anonymous=False)
 
-    global _we_are_home
-    _we_are_home = rospy.get_param('we_are_home', True)
+    global _team_side
+    param_name = rospy.search_param('team_side')
+    _team_side = rospy.get_param(param_name, 'home')
 
     # Sub/Pub
     global _state_pub
     _state_pub = rospy.Publisher('ball_state', BallState, queue_size=10)
     
     rospy.Subscriber('vision_position', Pose2D, _handle_vision_ball_position)
+    rospy.Subscriber('/game_state', GameState, _handle_game_state)
     
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
--- nodes/estimation/robot_estimator_node.py
+++ nodes/estimation/robot_estimator_node.py
@@ -17,14 +17,22 @@ plumbing is done in the `robot.launch` file.
 """
 
 _state_pub = None
-_we_are_home = True
+_team_side = None
+
+# Again, this could be done in the vision code
+from soccerref.msg import GameState
+_game_state = GameState
+
+def _handle_game_state(msg):
+    global _game_state
+    _game_state = msg
 
 def _handle_vision_position(msg):
     # Flip the coordinate system so that our side is always the negative
     # side. This is nice because we won't have to think about this later
     # in our high-level AI code and elsewhere.
     # This could also be done in the vision node; see original mcthuggets code
-    if not _we_are_home:
+    if (_team_side != 'home') ^ bool(_game_state.second_half):
         msg.x = -1*msg.x
         msg.y = -1*msg.y
 
@@ -43,17 +51,17 @@ def _handle_vision_position(msg):
 def main():
     rospy.init_node('robot_estimator', anonymous=False)
 
-    global _we_are_home
-    # probably a better way than doing this...
-    ns = rospy.get_namespace().split('/')[1]
-    _we_are_home = rospy.get_param("/{}/we_are_home".format(ns), True)
+    global _team_side
+    param_name = rospy.search_param('team_side')
+    _team_side = rospy.get_param(param_name, 'home')
 
     # Sub/Pub
-    global _state_pub
+    global _state_pub, _game_state
     # Use remap in roslaunch file to create separate channels per robot
     _state_pub = rospy.Publisher('robot_state', RobotState, queue_size=10)
 
     rospy.Subscriber('vision_position', Pose2D, _handle_vision_position)
+    rospy.Subscriber('/game_state', GameState, _handle_game_state)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
